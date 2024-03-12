082b2ccd412b07da15eec248cc9a226564be84e1
algodoncin <105017985+algodoncin@users.noreply.github.com.>
2024-03-12 13:56:12 -0600

    points algorithm added, for loop added

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let getUserChoice = prompt("Enter your choice between Rock, Paper or Scissor: ");
-
 const getComputerChoice = () => {
 
     let num = Math.floor(Math.random() * 3) + 1;
@@ -22,8 +20,8 @@ const getComputerChoice = () => {
 const gameRound = (playerSelection, computerSelection) => {
     let player = playerSelection.toLowerCase()
     let computer = computerSelection.toLowerCase();
-    console.log(player);
     let result = "";
+    let resultNum = 0;
  
     if(player != "paper" && player != "rock" && player != "scissor"){
         result = `${playerSelection} is not a valid choice`;
@@ -33,42 +31,51 @@ const gameRound = (playerSelection, computerSelection) => {
             case "rock":
                 if(computer == "paper"){
                     result = `You lose! ${playerSelection} (You) loses against ${computerSelection} (Computer)`;
+                    resultNum = 0;
                 }
                 else if(computer == "scissor"){
                     result = `You win! ${playerSelection} (You) beats ${computerSelection} (Computer)`;
+                    resultNum = 1;
                 }
                 else if(computer == "rock"){
                     result = `${playerSelection} (You) ties ${computerSelection} (Computer)`;
+                    resultNum = 2;
                 }
                 break;
             case "paper":
                 if(computer == "scissor"){
                     result = `You lose! ${playerSelection} (You) loses against ${computerSelection} (Computer)`;
+                    resultNum = 0;
                 }
                 else if(computer == "rock"){
                     result = `You win! ${playerSelection} (You) beats ${computerSelection} (Computer)`;
+                    resultNum = 1;
                 }
                 else if(computer == "paper"){
                     result = `${playerSelection} (You) ties ${computerSelection} (Computer)`;
+                    resultNum = 2;
                 }
                 break;
             case "scissor":
                 if(computer == "rock"){
                     result = `You lose! ${playerSelection} (You) loses against ${computerSelection} (Computer)`;
+                    resultNum = 0;
                 }
                 else if(computer == "paper"){
                     result = `You win! ${playerSelection} (You) beats ${computerSelection} (Computer)`;
+                    resultNum = 1;
                 }
                 else if(computer == "scissor"){
                     result = `${playerSelection} (You) ties ${computerSelection} (Computer)`;
+                    resultNum = 2;
                 }
                 break;
         }
     }
 
-    
+    let response = [result, resultNum];
 
-    return result
+    return response
 }
 
 let i = 0;
@@ -76,6 +83,21 @@ let playerScore = 0;
 let PCScore = 0;
 
 for(i ; i < 5 ; i++){
-    console.log(i);
+    let getUserChoice = prompt("Enter your choice between Rock, Paper or Scissor: ");
+    let roundResult = gameRound(getUserChoice, getComputerChoice());
+
+    console.log(`Round ${i+1}: ${roundResult[0]}`);
+
+    if(roundResult[1] == 0){
+        PCScore = PCScore + 1;
+    }
+    else if(roundResult[1] == 2){
+        i = i - 1;
+        console.log("Let's repeat the round");
+    }
+    else if(roundResult[1] == 1){
+        playerScore = playerScore + 1;
+    }
 }
-console.log(gameRound(getUserChoice, getComputerChoice())); 
+
+console.log(`Final scores are: Player ${playerScore} and PC ${PCScore}`);
