794f5313a1730861ae9f5a06759bde6a37b34a27
algodoncin <105017985+algodoncin@users.noreply.github.com.>
2024-03-12 09:57:56 -0600

    PC choice

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello assholes");
+const getComputerChoice = () => {
+
+    let num = Math.floor(Math.random() * 3) + 1;
+
+    let choice = "";
+
+    if(num == 1){
+        choice = "Rock";
+    }
+    else if(num == 2){
+        choice = "Paper";
+    }
+    else{
+        choice = "Scissor"
+    }
+
+    return choice
+}
+
+console.log("PC chose: " + getComputerChoice()); 
