178993d5c21de02edb87e220e47e77fe3b07cecb
algodoncin <105017985+algodoncin@users.noreply.github.com.>
2024-03-12 13:31:22 -0600

    game round works

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let getUserChoice = prompt("Enter your choice between Rock, Paper or Scissor: ");
+
 const getComputerChoice = () => {
 
     let num = Math.floor(Math.random() * 3) + 1;
@@ -17,4 +19,63 @@ const getComputerChoice = () => {
     return choice
 }
 
-console.log("PC chose: " + getComputerChoice()); 
+const gameRound = (playerSelection, computerSelection) => {
+    let player = playerSelection.toLowerCase()
+    let computer = computerSelection.toLowerCase();
+    console.log(player);
+    let result = "";
+ 
+    if(player != "paper" && player != "rock" && player != "scissor"){
+        result = `${playerSelection} is not a valid choice`;
+    }
+    else{
+        switch(player){
+            case "rock":
+                if(computer == "paper"){
+                    result = `You lose! ${playerSelection} (You) loses against ${computerSelection} (Computer)`;
+                }
+                else if(computer == "scissor"){
+                    result = `You win! ${playerSelection} (You) beats ${computerSelection} (Computer)`;
+                }
+                else if(computer == "rock"){
+                    result = `${playerSelection} (You) ties ${computerSelection} (Computer)`;
+                }
+                break;
+            case "paper":
+                if(computer == "scissor"){
+                    result = `You lose! ${playerSelection} (You) loses against ${computerSelection} (Computer)`;
+                }
+                else if(computer == "rock"){
+                    result = `You win! ${playerSelection} (You) beats ${computerSelection} (Computer)`;
+                }
+                else if(computer == "paper"){
+                    result = `${playerSelection} (You) ties ${computerSelection} (Computer)`;
+                }
+                break;
+            case "scissor":
+                if(computer == "rock"){
+                    result = `You lose! ${playerSelection} (You) loses against ${computerSelection} (Computer)`;
+                }
+                else if(computer == "paper"){
+                    result = `You win! ${playerSelection} (You) beats ${computerSelection} (Computer)`;
+                }
+                else if(computer == "scissor"){
+                    result = `${playerSelection} (You) ties ${computerSelection} (Computer)`;
+                }
+                break;
+        }
+    }
+
+    
+
+    return result
+}
+
+let i = 0;
+let playerScore = 0;
+let PCScore = 0;
+
+for(i ; i < 5 ; i++){
+    console.log(i);
+}
+console.log(gameRound(getUserChoice, getComputerChoice())); 
